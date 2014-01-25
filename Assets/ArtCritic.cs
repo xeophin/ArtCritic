d62258d7d8cca89bbcd115c22bf3162d3b2d9402
Kaspar Manz <kaspar@xeophin.net>
2014-01-25 10:56:25 +0100

    Eigenschaften der GameObjects

---
+++ Assets/ArtCritic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArtCritic : MonoBehaviour
+{
+  Transform art;
+  public GameObject obj;
+  // Use this for initialization
+  void Start ()
+  {
+    art = GameObject.Find ("art").transform;
+    Destroy (this, 10);
+  }
+  // Update is called once per frame
+  void Update ()
+  {
+    Instantiate (obj);
+  }
+}
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class Test : MonoBehaviour
 {
-		// Use this for initialization
-		void Start ()
-		{
+  // Use this for initialization
+  void Start ()
+  {
 	
-		}
-		// Update is called once per frame
-		void Update ()
-		{
-				transform.position = new Vector3 (transform.position.x + 0.01f, transform.position.y);
-		}
+  }
+  // Update is called once per frame
+  void Update ()
+  {
+    transform.position = new Vector3 (transform.position.x + 0.01f, transform.position.y);
+  }
 }
