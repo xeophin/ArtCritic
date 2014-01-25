22d2a0e54432e8b14884242a1e245bb05953bddf
Kaspar Manz <kaspar@xeophin.net>
2014-01-25 18:20:56 +0100

    Champagne! Champagne for EVERYONE!

--- Assets/FloorArea.cs
+++ Assets/FloorArea.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FloorArea : MonoBehaviour {
-	public bool OnFloor(int gridX, int gridY, float gridSize) {
-		return OnFloor (gridX * gridSize, gridY * gridSize);
-	}
+public class FloorArea : MonoBehaviour
+{
+  public bool OnFloor (int gridX, int gridY, float gridSize)
+  {
+    return OnFloor (gridX * gridSize, gridY * gridSize);
+  }
 
-	public bool OnFloor(float x, float y) {
-		return renderer.bounds.min.x <= x && renderer.bounds.max.x > x && renderer.bounds.min.y <= y && renderer.bounds.max.y > y;
-	}
+  public bool OnFloor (float x, float y)
+  {
+    return renderer.bounds.min.x <= x && renderer.bounds.max.x > x && renderer.bounds.min.y <= y && renderer.bounds.max.y > y;
+  }
+
+  public Vector3 GetRandomPositionOnFloor ()
+  {
+    float x = Random.Range (renderer.bounds.min.x, renderer.bounds.max.x);
+    float y = Random.Range (renderer.bounds.min.y, renderer.bounds.max.y);
+    return new Vector3 (x, y, 0);
+  }
 }
