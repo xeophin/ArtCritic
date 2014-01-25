bcbb398a02a966738749680ce0848a3ecba3fcd1
Zarkonnen <david.stark@zarkonnen.com>
2014-01-25 17:24:17 +0100

    Floors and restricting movement on that basis.

--- Assets/MoveToPositionOnClick.cs
+++ Assets/MoveToPositionOnClick.cs
@@ -8,6 +8,7 @@ using UnityEditor;
 /// </summary>
 public class MoveToPositionOnClick : MonoBehaviour
 {
+	public GameObject floor;
   Vector3 target;
   // Update is called once per frame
   void Update ()
@@ -15,7 +16,11 @@ public class MoveToPositionOnClick : MonoBehaviour
     if (Input.GetMouseButtonUp (0)) {
       target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
       target = new Vector3 (target.x, target.y, 0);
-      iTween.MoveTo (this.gameObject, target, 1f);
+		if (floor != null && floor.GetComponent<FloorArea> ().OnFloor(target.x, target.y)) {
+      		iTween.MoveTo (this.gameObject, target, 1f);
+		} else {
+				iTween.ShakePosition(this.gameObject, new Vector3(0.2f, 0, 0), 0.5f);
+		}
     }
   }
 }
