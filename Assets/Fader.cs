6d8e5337effc184d4c1f15e9cd7ed9db360e8ce9
Kaspar Manz <kaspar@xeophin.net>
2014-01-26 10:27:42 +0100

    Added SplashScreen level and a fade mechanism to switch between levels.

---
+++ Assets/Fader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Fader : MonoBehaviour
+{
+  // Use this for initialization
+  void Start ()
+  {
+    iTween.CameraFadeAdd ();
+    iTween.CameraFadeFrom (1f, 3f);
+  }
+
+  IEnumerator LoadNextLevel ()
+  {
+    iTween.CameraFadeTo (0f, 3f);
+    yield return new WaitForSeconds (3f);
+    Application.LoadLevel (Application.loadedLevel + 1);
+  }
+}
