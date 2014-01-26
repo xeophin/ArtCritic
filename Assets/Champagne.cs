dd7f78ab81a274ceaee55ca317200174b0b89e03
Zarkonnen <david.stark@zarkonnen.com>
2014-01-26 12:46:38 +0100

    Champagne slurping sound.

--- Assets/Champagne.cs
+++ Assets/Champagne.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class Champagne : MonoBehaviour
 {
+	public AudioClip drink;
+
   public float drunkenness = 0;
   public FloorArea floor;
 
@@ -35,7 +37,7 @@ public class Champagne : MonoBehaviour
       // Go to another place
       Vector3 goTo = floor.GetRandomPositionOnFloor ();
       iTween.MoveTo (this.gameObject, goTo, 2f);
-
+		if (drink != null) { GetComponent<AudioSource> ().PlayOneShot (drink); }
     }
 
     GUI.matrix = oldMatrix;
